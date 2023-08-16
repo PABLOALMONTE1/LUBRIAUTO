65b29fadcc86d2dade7e7ecb7c335c14fa1a7af5
PABLOALMONTE1 <142457282+PABLOALMONTE1@users.noreply.github.com>
2023-08-16 19:14:52 -0400

    LUBRIAUTO.c mod. 1

--- LUBRIAUTO.c
+++ LUBRIAUTO.c
@@ -1,2 +1,12 @@
+/*------------------------------------------------------------------------------------|
+|  Autor:PABLO ALMONTE                    MATRICULA: 1-20-2318                        | 
+|         HOSNY CEBALLOS                             1-20-2085                        |
+|  Fecha: 16/08/2023                     Version: 1.00		                            |
+|-------------------------------------------------------------------------------------|           
+|  Descripcion del Programa: ESTE ES UN SISTEMA DE FACTURACION PARA UN LUBRIAUTO,     |
+                             DONDE PODREMOS FACTURAR Y COTIZAR NUESTROS PRODUCTOS Y   |
+                            SERVICIOS.                                                |   
+                                                                                      |           
+| -----------------------------------------------------------------------------------*/
 
 
